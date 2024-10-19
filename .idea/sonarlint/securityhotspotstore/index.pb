646c082d2327e335e7692a8175f116bd4e9b1f19
komob <b.komorniczak@gmail.com>
2024-10-19 13:48:12 +0200

    Add rethrowing

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,17 @@
+
+@
+build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
+P
+ src/main/java/SensitiveData.java,8/b/8b005e728a69ae861f2e8aee1979a79e5462ace6
+Z
+*src/main/java/CustomSerializationUtil.java,c/3/c3c5de693854e8ce0c84f62f15c396ae5103d2a4
+D
+src/main/java/a.java,3/3/33d494c2fc561df2fc31d8dc7fdfebaff7372cf0
+G
+src/main/java/Main.java,e/4/e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
+_
+/src/main/java/zad1/CustomSerializationUtil.java,2/b/2b049fdef04f120e62984b469e094aec921d0a91
+U
+%src/main/java/zad1/SensitiveData.java,7/6/76d76021859b3c0109e2e1fb02de3970877d2c28
+L
+src/main/java/zad1/Main.java,7/d/7d73dbcb1cf1d23b9c719c5c7f15b9a3e86ba63c
--- src/main/java/zad1/CustomSerializationUtil.java
+++ src/main/java/zad1/CustomSerializationUtil.java
@@ -1,40 +1,61 @@
 package zad1;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class CustomSerializationUtil {
 
+    private CustomSerializationUtil(){}
     private static final String SECRET_KEY = "1234567890123456";
 
-    private static String encrypt(String data) throws Exception {
+    private static String encrypt(String data)  {
         SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encryptedData = cipher.doFinal(data.getBytes());
+
+        byte[] encryptedData;
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            encryptedData = cipher.doFinal(data.getBytes());
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
         return Base64.getEncoder().encodeToString(encryptedData);
     }
 
-    private static String decrypt(String encryptedData) throws Exception {
+    private static String decrypt(String encryptedData)  {
         SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
-        byte[] decryptedData = cipher.doFinal(decodedData);
+        byte[] decryptedData;
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+             decryptedData = cipher.doFinal(decodedData);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
         return new String(decryptedData);
     }
 
-    public static void serialize(SensitiveData obj, String filePath) throws Exception {
+    public static void serialize(SensitiveData obj, String filePath) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
             oos.writeObject(obj.getName());
             oos.writeObject(encrypt(obj.getPesel()));
             oos.writeObject(encrypt(obj.getPassword()));
+        } catch (RuntimeException | IOException e ) {
+            throw new RuntimeException(e);
         }
     }
 
-    public static SensitiveData deserialize(String filePath) throws Exception {
+    public static SensitiveData deserialize(String filePath) {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
             String name = (String) ois.readObject();
             String encryptedPesel = (String) ois.readObject();
@@ -44,6 +65,8 @@ public class CustomSerializationUtil {
             String password = decrypt(encryptedPassword);
 
             return new SensitiveData(name, pesel, password);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
